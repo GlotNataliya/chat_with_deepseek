0aa6031bd0e5ccd22f625c33329f686fd2d9a79a
GlotNataliya <46719802+GlotNataliya@users.noreply.github.com>
2025-03-24 20:36:21 +0300

    Correcting tooltip

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -3,12 +3,26 @@ import { Tooltip } from "bootstrap";
 
 export default class extends Controller {
   connect() {
-    this.tooltip = new Tooltip(this.element)
+    const link = this.element;
+    this.tooltip = new Tooltip(link);
+
+    link.addEventListener('mouseenter', () => {
+      this.tooltip.show();
+    });
+
+    link.addEventListener('mouseleave', () => {
+      this.tooltip.hide();
+    });
   }
 
   disconnect() {
     if (this.tooltip) {
-      this.tooltip.dispose()
+      this.tooltip.dispose();
+
+      const link = this.element;
+
+      link.removeEventListener('mouseenter', this.tooltip.show);
+      link.removeEventListener('mouseleave', this.tooltip.hide);
     }
   }
 }
